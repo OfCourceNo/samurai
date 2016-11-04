7ceb5ebac86a65537274a74e0447b32834f751d9
kicchi <kicchidayo@gmail.com>
2016-11-04 19:42:29 +0900

    testdayo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(void) {
 // HELLO WORLDを書きました。カタカナ
 	std::cout << "HelloWorld!!" << std::endl; 
 	std::cout << "こんにちわ、おバカちゃん" << std::endl; 
+	std::cout << "こんにちわ、ゆうせいくん" << std::endl; 
 	
 	return 0;
 }

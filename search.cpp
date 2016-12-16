e7292ad06b21b97e7b09df95b7c8c1bca2293bbd
kikuchi shoma <kikuchishoma@kikuchi-no-MacBook-Air.local>
2016-12-16 12:00:16 +0900

    修正

--- search.cpp
+++ search.cpp
@@ -3,7 +3,6 @@
 using std::vector;
 using std::cout;
 using std::endl;
-#define INF 1000000
 
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
@@ -11,9 +10,9 @@ typedef vector<vector<int>> vvi;
 
 
 //field情報の出力関数
-void vv_print( vector< vector<int> > v){
-	for (int i = 4; i < 19; i++){
-		for (int j = 4; j < 19; j++){
+void vv_print(vvi v){
+	for (int i = 0; i < 15; i++){
+		for (int j = 0; j < 15; j++){
 			cout << v[i][j] ;
 		}
 		cout << endl;
@@ -22,23 +21,13 @@ void vv_print( vector< vector<int> > v){
 
 //二次元配列の０初期化
 void init(vvi &v){
-	for (int i = 0; i < 23; i++){
-		for (int j = 0; j < 23; j++){
-			if (i < 4 or 18 < i){
-				v[i][j] = INF;
-			}else if ((4 <= i or i <= 18) and (j < 4 or 18 < j)){
-				v[i][j] = INF;
-			}else{
-				v[i][j] = 0;
-			}
-		}
-	}
+	v = vvi(15, vi(15,0));
 }
 
 //0check
 bool check0(vvi v){
-	for (int i = 4; i < 19; i++){
-		for (int j = 4; j < 19; j++){
+	for (int i = 0 ;i < 15; i++){
+		for (int j = 0; j < 15; j++){
 			if(v[i][j] > 0){
 				return false;
 			}
@@ -62,10 +51,10 @@ void masakari(int px,int py,vvi &tmp,int n){
 	int dy[7] = {-1, -1, -1, 0, 0, 1, 1};
 	for (int j = 0 ; j < 7; j++){
 		int tmpx = dx[j],tmpy = dy[j];
-		rotation(tmpx,tmpy,n);
-		tmpx += px + 1;
-		tmpy += py + 1;
-		if (tmp[tmpx][tmpy] == 1){
+		ation(tmpx,tmpy,n);
+		tmpx += px;
+		tmpy += py;
+		if (0 <= tmpx and tmpx < 15 and 0 <= tmpy and tmpy < 15 and tmp[tmpx][tmpy] == 1){
 			tmp[tmpx][tmpy] = 0;
 		}
 	}
@@ -77,10 +66,10 @@ void katana(int px,int py,vvi &tmp,int n){
 	int dy[6] = { 1, 1,-1, 0, 0};
 	for (int j = 0 ; j < 5; j++){
 		int tmpx = dx[j],tmpy = dy[j];
-		rotation(tmpx,tmpy,n);
-		tmpx += px + 1;
-		tmpy += py + 1;
-		if (tmp[tmpx][tmpy] == 1){
+		ation(tmpx,tmpy,n);
+		tmpx += px;
+		tmpy += py;
+		if (0 <= tmpx and tmpx < 15 and 0 <= tmpy and tmpy < 15 and tmp[tmpx][tmpy] == 1){
 			tmp[tmpx][tmpy] = 0;
 		}
 	}
@@ -92,10 +81,10 @@ void yari(int px,int py,vvi &tmp,int n){
 	int dy[4] = {1, 2, 3, 4};
 	for (int j = 0 ; j < 4; j++){
 		int tmpx = dx[j],tmpy = dy[j];
-		rotation(tmpx,tmpy,n);
+		ation(tmpx,tmpy,n);
 		tmpx += px;
 		tmpy += py;
-		if (tmp[tmpx][tmpy] == 1){
+		if (0 <= tmpx and tmpx < 15 and 0 <= tmpy and tmpy < 15 and tmp[tmpx][tmpy] == 1){
 			tmp[tmpx][tmpy] = 0;
 		}
 	}
@@ -103,22 +92,23 @@ void yari(int px,int py,vvi &tmp,int n){
 
 //masakariの存在場所列挙。existに結果格納
 void masakari_check(vvi chlo,vvi &exist){
-	vvi tmp(23,vi(23));
+	vvi tmp(15,vi(15));
 	init(tmp);
 	int pointx = 0, pointy = 0;
-	for (int rot = 0; rot < 4; rot++){
-		for (int pointx = 4; pointx < 19; pointx++){
-			for (int pointy = 4; pointy < 19; pointy++){
+	for (int rot  = 0; rot < 4; rot++){
+		for (int pointx = 0; pointx < 15; pointx++){
+			for (int pointy = 0; pointy < 15; pointy++){
 				int adx[3] = {1, -1, 0};
 				int ady[3] = {0, 0, -1};
 				tmp = chlo;
 				masakari(pointx,pointy,tmp,rot);
 				//とりあえず加算して見た。多分数字多ければ可能性も高い
 				if (check0(tmp)){
-					cout << "rot = " << rot << endl;
 					for (int i = 0; i < 3; i++){
 						rotation(adx[i],ady[i],rot);
-						exist[pointx+adx[i]+1][pointy+ady[i]+1] += 1;
+						if (0 <= pointx+adx[i] and pointx + adx[i] < 15 and 0 <= pointy+ady[i] and pointy + ady[i] < 15){
+						exist[pointx+adx[i]][pointy+ady[i]] += 1;
+						}
 					}
 				}
 			}
@@ -128,11 +118,11 @@ void masakari_check(vvi chlo,vvi &exist){
 }
 
 void katana_check(vvi chlo,vvi &exist){
-	vvi tmp(23,vi(23));
+	vvi tmp(15,vi(15));
 	int pointx = 0, pointy = 0;
 	for (int rot = 0; rot < 4; rot++){
-		for (int pointx = 4; pointx < 19; pointx++){
-			for (int pointy = 4; pointy < 19; pointy++){
+		for (int pointx = 0; pointx < 15; pointx++){
+			for (int pointy = 0; pointy < 15; pointy++){
 				int adx[2] = {0, 1};
 				int ady[2] = {1, 0};
 				tmp = chlo;
@@ -140,8 +130,10 @@ void katana_check(vvi chlo,vvi &exist){
 				//とりあえず加算して見た。多分数字多ければ可能性も高い
 				if (check0(tmp)){
 					for (int i = 0; i < 2; i++){
-						rotation(adx[i],ady[i],rot);
-						exist[pointx+adx[i]+1][pointy+ady[i]+1] += 1;
+						ation(adx[i],ady[i],rot);
+						if (0 <= pointx+adx[i] and pointx + adx[i] < 15 and 0 <= pointy+ady[i] and pointy + ady[i] < 15){
+						exist[pointx+adx[i]][pointy+ady[i]] += 1;
+						}
 					}
 				}
 			}
@@ -153,11 +145,11 @@ void katana_check(vvi chlo,vvi &exist){
 
 
 void yari_check(vvi chlo,vvi &exist){
-	vvi tmp(23,vi(23));
+	vvi tmp(15,vi(15));
 	int pointx = 0, pointy = 0;
 	for (int rot = 0; rot < 4; rot++){
-		for (int pointx = 4; pointx < 19; pointx++){
-			for (int pointy = 4; pointy < 19; pointy++){
+		for (int pointx = 0; pointx < 15; pointx++){
+			for (int pointy = 0; pointy < 15; pointy++){
 				int adx[1] = {0};
 				int ady[1] = {1};
 				tmp = chlo;
@@ -165,8 +157,10 @@ void yari_check(vvi chlo,vvi &exist){
 				//とりあえず加算して見た。多分数字多ければ可能性も高い
 				if (check0(tmp)){
 					for (int i = 0; i < 1; i++){
-						rotation(adx[i],ady[i],rot);
+						ation(adx[i],ady[i],rot);
+						if (0 <= pointx+adx[i] and pointx + adx[i] < 15 and 0 <= pointy+ady[i] and pointy + ady[i] < 15){
 						exist[pointx+adx[i]][pointy+ady[i]] += 1;
+						}
 					}
 				}
 			}
@@ -175,19 +169,20 @@ void yari_check(vvi chlo,vvi &exist){
 }
 
 int main(){
-	vvi change_locate(23,vi(23));
+	vvi change_locate(15,vi(15));
 	init(change_locate);
-	vvi exist(23,vi(23));
+	vvi exist(15,vi(15));
 	init(exist);
 
-	change_locate[10][10] = 1; 
-	change_locate[10][11] = 1; 
-	change_locate[9][11] = 1; 
-	change_locate[11][9] = 1; 
-	change_locate[11][10] = 1; 
+	//change_locate[10][10] = 1; 
+	//change_locate[10][11] = 1; 
+	//change_locate[9][11] = 1; 
+	//change_locate[11][9] = 1; 
+	//change_locate[11][10] = 1; 
+	change_locate[0][0] = 1; 
 	masakari_check(change_locate,exist);
-	katana_check(change_locate,exist);
-	yari_check(change_locate,exist);
+	//katana_check(change_locate,exist);
+	//yari_check(change_locate,exist);
 	cout << "change_loacte" << endl;
 	vv_print(change_locate);
 	cout << "exist" << endl;

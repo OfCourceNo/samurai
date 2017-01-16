7a3186e2b642ca0c02ba997aad60483900206355
naobird <nao.poketurtle@gmail.com>
2017-01-17 01:50:31 +0900

    player 一つ目

--- toriyabe/players/selfPlayer.cpp
+++ toriyabe/players/selfPlayer.cpp
@@ -1,8 +1,62 @@
 #include "players.hpp"
 
 struct SelfPlayer: Player {
-  void play(GameInfo& info) {
-  }
+    int attack(GameInfo& info, SamuraiState* samurai) {
+        int choicedAction = 0;
+        int getField = 0;
+        for (int action = 1; action <= 4; action ++) {
+            int territory, injury, hiding;
+            info.tryAction(samurai, action, undo, territory, injury, hiding);
+            if (territory >= getField) {
+                choicedAction = action;
+                getField = territory;
+            }
+        }
+        return choicedAction;
+    }
+
+    int move(GameInfo& info, SamuraiState* samurai) {
+        int x = samurai->x;
+        int y = samurai->y;
+        if (field[x][y - 1] >= 3) {
+            return 7;
+        } else if (field[x - 1][y] >= 3) {
+            return 8;
+        } else if (field[x][y + 1] >= 3) {
+            return 5;
+        } else if (field[x + 1][y] >= 3) {
+            return 6;    
+        } else {
+            return 5;
+        }
+    }
+
+    void play(GameInfo& info) {
+        SamuraiState* samurai;
+        int choicedPlayer = currentTurn % 6 / 2;
+        int action;
+        samurai = &samuraiStates[0][choicedPlayer];
+        cout << samurai->weapon;
+        if (samurai->hidden == 0) {
+            action = attack(SamuraiState* samurai);  
+            cout << ' ' << action;
+            action += 4;
+            cout << ' ' << action;
+            action = 9;
+            if(info.isValid(*samurai, action)) {
+                cout << ' ' << action;
+            }
+        } else {
+            action = 9;
+            cout << ' ' << action;
+            action = move(SamuraiState* samurai);  
+            cout << ' ' << action;
+            cout << ' ' << action;
+            cout << ' ' << action;
+        }
+        cout << " 0" << endl;
+        cout.flush();
+    }
 };
 
 Player* player = new SelfPlayer();
